b4187c1edd7544eb6eaa328aab31347e9a554473
hsam98023-tech <hsam98023@gmail.com>
2026-02-17 00:53:54 +0000

    Update Particles.tsx

--- Particles.tsx
+++ Particles.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useMemo } from 'react';
 import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
-import { useStore } from '../store';
+import { useStore } from '@/store';
 
 interface ParticlesProps {
   count?: number;
